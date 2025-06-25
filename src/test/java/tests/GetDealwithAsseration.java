94cbe9af3470122d65711b64e25e7404cf66456d
ign-hiren157 <hiren.prajapati@ignitiongroup.co.za>
2025-06-25 15:58:24 +0530

    Get Deal code change

--- src/test/java/tests/GetDealwithAsseration.java
+++ src/test/java/tests/GetDealwithAsseration.java
@@ -1,88 +1,92 @@
 package tests;
 
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-
+import io.restassured.RestAssured;
 import io.restassured.response.Response;
 
-//import io.restassured.matcher.ResponseAwareMatcher;
-//import io.restassured.response.Response;
-import static org.hamcrest.Matchers.equalTo;
-import static io.restassured.RestAssured.given;
-
-
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.equalTo;
+
 import org.testng.Assert;
 
 public class GetDealwithAsseration {
-	
-	@Test (priority = 1)
+
+	private static final String BASE_URI 		= "http://t2.vas.api.ignitiongroup.co.za/vas/";
+	private static final String DEAL_PATH 		= "Safebase/GetAvailableDeals";
+	private static final String VALID_USERNAME 	= "VasAPI";
+	private static final String VALID_PASSWORD 	= "Test@123";
+
+	@BeforeClass
+	public void setup() {
+		
+		RestAssured.baseURI = BASE_URI;
+	}
+
+	@Test(priority = 1)
 	public void testValidCredentialsAndValidPath() {
-	    baseURI = "http://t2.vas.api.ignitiongroup.co.za/vas/";
-	    String dealpath = "Safebase/GetAvailableDeals";
 
-	    //Response response = 
-	    given()
-	        .auth()
-	        .preemptive()
-	        .basic("VasAPI", "Test@123")
-	    .when()
-	        .get(dealpath)
-	    .then()
-	        .statusCode(200)
-	        .body("Message", equalTo("Deals returned successfully."))
-	        .extract().response();
-	    //System.out.println("Response:\n" + response.prettyPrint());
-}
+		Response response = 
+				            given().
+							auth().
+							preemptive().
+							basic(VALID_USERNAME, VALID_PASSWORD).
+							when().
+							get(DEAL_PATH)
+							.then().
+							statusCode(200).
+							body("Message", equalTo("Deals returned successfully.")).
+							extract().
+							response();
+							//System.out.println(response.asString());
+				            System.out.println("Response:\n" + response.prettyPrint());
+	}
 
-	@Test (priority = 2)
+	@Test(priority = 2)
 	public void testInvalidCredentials() {
-	    baseURI = "http://t2.vas.api.ignitiongroup.co.za/vas/";
-	    String dealpath = "Safebase/GetAvailableDeals";
-
-	    given()
-	        .auth()
-	        .preemptive()
-	        .basic("WrongUser", "WrongPass")
-	    .when()
-	        .get(dealpath)
-	    .then()
-	        .statusCode(200)
-	        .body("Message", equalTo("The Api User could not be found or has not been Authenticated."));
+		
+							given().
+							auth().
+							preemptive().
+							basic("WrongUser", "WrongPass").
+							when().
+							get(DEAL_PATH).
+							then().
+							statusCode(200).
+							body("Message", equalTo("The Api User could not be found or has not been Authenticated."));
 	}
 
-	@Test (priority = 3, enabled = false)
+	@Test(priority = 3, enabled = false)
 	public void testInvalidPath() {
-	    baseURI = "http://t2.vas.api.ignitiongroup.co.za/vas/";
-	    String invalidPath = "Safebase/GetAvailableDealsInvalid";
 
-	    given()
-	        .auth()
-	        .preemptive()
-	        .basic("VasAPI", "Test@123")
-	    .when()
-	        .get(invalidPath)
-	    .then()
-	        .statusCode(500)
-	        .body("Message", equalTo("500 Internal Server Error"));
+		String invalidPath = "Safebase/GetAvailableDealsInvalid";
+
+							given().
+							auth().
+							preemptive().
+							basic(VALID_USERNAME, VALID_PASSWORD).
+							when().
+							get(invalidPath).
+							then().
+							statusCode(500).
+							body("Message", equalTo("500 Internal Server Error"));
 	}
 
-	@Test (priority = 4, enabled = true)
+	@Test(priority = 4, enabled = true)
 	public void testResponseNotEmpty() {
-	    baseURI = "http://t2.vas.api.ignitiongroup.co.za/vas/";
-	    String dealpath = "Safebase/GetAvailableDeals";
 
-	    String responseBody = given()
-	        .auth()
-	        .preemptive()
-	        .basic("VasAPI", "Test@123")
-	    .when()
-	        .get(dealpath)
-	        .then()
-	        .statusCode(200)
-	        .extract()
-	        .asString();
+		String responseBody = given().
+							  auth().
+							  preemptive().
+							  basic(VALID_USERNAME, VALID_PASSWORD).
+							  when().
+							  get(DEAL_PATH).
+							  then().
+							  statusCode(200).
+							  extract().
+							  asString();
 
-	    Assert.assertFalse(responseBody.trim().isEmpty(), "Response body should not be empty");
+		Assert.assertFalse(responseBody.trim().isEmpty(), "Response body should not be empty");
 	}
 
 }
